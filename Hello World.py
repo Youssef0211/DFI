a6c76ffc1331fa0c06729caac2b5af25cc69412b
Youssef Roushdy <youssef_roshdy@hotmail.com>
2021-05-31 19:20:36 +0200

    learning from float to int

--- Hello World.py
+++ Hello World.py
@@ -1,11 +1,14 @@
 print("Hello World")
 
 x=5
-y=0
+y=9
 
 z = x+y
 
 if (z > 5):
     print("Z greater than 5")
+    w = z/2
+    print ("w is: " + str(int(w)))
 else:
-    print ("Z is less than 5")
+    print ("Z not greater than 5")
+
