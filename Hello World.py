adf0b0803473b67fd535ecac4e913c0816ec1544
Youssef Roushdy <youssef_roshdy@hotmail.com>
2021-05-31 20:33:57 +0200

    try new app for heroku

--- Hello World.py
+++ Hello World.py
@@ -1,8 +1,20 @@
-from flask import Flask
+import flask
+import os
+from flask import send_from_directory
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
